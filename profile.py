087af651ce7f15df52b2710e48739cf44a76cd84
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-09-18 17:04:53 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -3,26 +3,34 @@ import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
 
-# Create a portal context.
+# Create a portal context
 pc = portal.Context()
+
+# Describes the parameters // section 8.8
+portal.context.defineParameter( "n", "Number of VMs", portal.ParametreType.Integer, 1)
+
+# Retrieves the values the user inputs // section 8.8
+usr = portal.context.bindParameters() 
 
 # Create a Request object to start building the RSpec
 request = pc.makeRequestRSpec()
  
-# Creates number of nodes specified by the user and also sets node-1 to have a public ID address
-for i in range(4):
-    node = request.XenVM("node-" + str(i))  
-
-# Sets each node to run CentOS7-64-STD
-for i in range(4):
-    node.disk_image = "urn:publicide:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-
-# Sets each node to have their respected local IP address
-for i in range(4):
-    node.addAddress(rspec.IPv4Address("192.168.1."+str(i), "255.255.255.0"))
-
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-
+# Presents and error to the user if the value is not within the boundary // section 8.8
+if usr.n <1 or usr.n >4: 
+    pc.reportError( portal.ParameterError("Value inputed must be between 1 and 4")
+                   
+link = request.LAN("lan")
+                          
+# Creates number of nodes specified by the user 
+for i in range(usr.n):
+    node = request.XenVM("node-" + str(i+1)) # Creates number of nodes
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+    interface = node.addInterface("iface"+str(i+1))
+    interface.component_id = "eth"
+    interface.addAddress(rspec.IPv4Address("192.168.1."+str(i+1), "255.255.255.0")) # Sets each of the nodes to have their respected IP address
+    link.addInterface(iface)             
+    if (i+1) == 1
+          node.routable_control_ip = "true" # sets node-1 to have a public ID
+    
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec()

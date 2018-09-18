e58bee8b6fffbf5b3f26c44cd2f42ff35b3e9ad6
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-09-18 16:11:39 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -20,17 +20,17 @@ if params.n < 1 or params.n >8:
     portal.context.reportError( portal.ParameterError( "Number of VMs is incorrect. Must be between 1 and 8")) 
  
 # Creates number of nodes specified by the user and also sets node-1 to have a public ID address
-for i in range(params.n)
+for i in range(params.n):
     node = request.XenVM("node-" + str(i))
 
-node-1.routable
-   
+node-1.routable_control_ip = "true"    
+
 # Sets each node to run CentOS7-64-STD
-for i in range(params.n)
+for i in range(params.n):
     node.disk_image = "urn:publicide:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 # Sets each node to have their respected local IP address
-for i in range(params.n)
+for i in range(params.n):
     node+str(i).addAddress(rspec.IPv4Address("192.168.1."+str(i), "255.255.255.0"))
 
 # Install and execute a script that is contained in the repository.
